39f9079809d0d16ba9dd64f52e156dacebd8a9bf
Wilber Quintero <wilber.valdez247180@potros.itson.edu.mx>
2025-06-19 17:08:49 -0700

    Initial commit

---
+++ app/src/main/java/yocupicio/martin/examen2_yocupiciomartin/Alumno.kt
@@ -0,0 +1,8 @@
+package yocupicio.martin.examen2_yocupiciomartin
+
+data class Alumno: Boolean(){ var nombre: String,
+    var apellido: String,
+    var carrera: String,
+    var email: String
+
+}
